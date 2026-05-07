cf7c710a5d36fc73b5a6081378e4e36801cef962
Rukmini <rukminidimple16@gmail.com>
2026-05-07 22:00:45 +0530

    added resume upload feature

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import PyPDF2
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "AI Resume Analyzer Running Successfully"
+
+    extracted_text = ""
+
+    if request.method == "POST":
+
+        pdf_file = request.files["resume"]
+
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        for page in pdf_reader.pages:
+            extracted_text += page.extract_text()
+
+    return render_template("index.html", text=extracted_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
